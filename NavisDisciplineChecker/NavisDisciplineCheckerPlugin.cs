9912a301b649e94d3d2a6afedc7ff6e7679ce9b1
budaevaler <valeria.budaeva@yandex.ru>
2022-02-25 11:00:56 +0300

    Добавлен плагин для проверки nwf-файлов по разделам

---
+++ NavisDisciplineChecker/NavisDisciplineCheckerPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace NavisDisciplineChecker {
+
+    [Plugin("NavisDisciplineChecker", "budaevaler", DisplayName = "Проверка nwf", ToolTip = "Проверка nwf по разделам")]
+    public class NavisDisciplineCheckerPlugin : AddInPlugin {
+        public override int Execute(params string[] parameters) {
+            return 0;
+        }
+    }
+}
