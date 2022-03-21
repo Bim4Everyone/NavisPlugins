cff21787414b09a5e2f5efb56b94e48195bc1aff
dosymep <dosymep@gmail.com>
2022-03-21 09:56:49 +0300

    Форматировал атрибут

--- NavisDisciplineChecker/NavisDisciplineCheckerPlugin.cs
+++ NavisDisciplineChecker/NavisDisciplineCheckerPlugin.cs
@@ -7,7 +7,8 @@ using Autodesk.Navisworks.Api.Plugins;
 
 namespace NavisDisciplineChecker {
 
-    [Plugin("NavisDisciplineChecker", "budaevaler", DisplayName = "Проверка nwf", ToolTip = "Проверка nwf по разделам")]
+    [Plugin("NavisDisciplineChecker", "budaevaler", 
+        DisplayName = "Проверка nwf", ToolTip = "Проверка nwf по разделам")]
     public class NavisDisciplineCheckerPlugin : AddInPlugin {
         public override int Execute(params string[] parameters) {
             CombineDisciplineFiles();
