1218a144e26887ab5a05d914c6d997f414bee27a
dosymep <dosymep@gmail.com>
2022-03-22 12:33:49 +0300

    Добавил команду создания отчетов коллизий

---
+++ NavisDisciplineChecker/NavisCreateClashReportPlugin.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.Navisworks.Api.Plugins;
+
+namespace NavisDisciplineChecker {
+    [Plugin("NavisCreateClashReportPlugin", "budaevaler", 
+        DisplayName = "Создать отчет коллизий", ToolTip = "Создает отчет коллизий")]
+    public class NavisCreateClashReportPlugin : AddInPlugin {
+        public override int Execute(params string[] parameters) {
+            CreateClashReportPlugin();
+            return 0;
+        }
+
+        private void CreateClashReportPlugin() {
+            
+        }
+    }
+}
