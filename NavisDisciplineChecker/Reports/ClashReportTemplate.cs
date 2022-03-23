d7f1672eaa28c447711e35dc7feaef3e7c634690
dosymep <dosymep@gmail.com>
2022-03-23 11:59:32 +0300

    Добавил отчет

---
+++ NavisDisciplineChecker/Reports/ClashReportTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+using DevExpress.XtraReports.UI;
+
+namespace NavisDisciplineChecker.Reports {
+    public partial class ClashReportTemplate : DevExpress.XtraReports.UI.XtraReport {
+        public ClashReportTemplate() {
+            InitializeComponent();
+        }
+
+    }
+}
