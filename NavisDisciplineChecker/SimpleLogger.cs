4b9378ad2e23053fba553e07964166b3787a08e1
dosymep <dosymep@gmail.com>
2022-04-07 16:57:53 +0300

    Добавил простой логгер

---
+++ NavisDisciplineChecker/SimpleLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace NavisDisciplineChecker {
+    public class SimpleLogger : IDisposable {
+        private readonly StreamWriter _stream;
+
+        public SimpleLogger(string fileName) {
+            _stream = File.CreateText(fileName);
+        }
+
+        public void WriteLine(string message) {
+            _stream.WriteLine(message);
+        }
+
+        #region IDisposable
+
+        protected virtual void Dispose(bool disposing) {
+            if(disposing) {
+                _stream.Flush();
+                _stream.Dispose();
+            }
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
